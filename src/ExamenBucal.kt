7b7926ac96dc5119a90e756946a7af7a6f860d57
MrLainez <koris0412@gmail.com>
2026-06-06 13:50:45 -0600

    feat: agregar data classes para expediente clínico y odontograma

---
+++ src/ExamenBucal.kt
@@ -0,0 +1,13 @@
+class ExamenBucal (
+    val id: Int? = null,
+    val pacienteId: Int,
+    val labios: String?,
+    val lengua: String?,
+    val encia: String?,
+    val carrillos: String?,
+    val pisaBoca: String?,
+    val fSaco: String?, // Fondo de Saco
+    val pDuro: String?, // Paladar Duro
+    val pBlando: String?, // Paladar Blando
+    val observaciones: String?
+)
---
+++ src/Paciente.kt
@@ -0,0 +1,13 @@
+class Paciente (
+    val id: Int? = null,
+    val nombre: String,
+    val edad: Int,
+    val direccion: String,
+    val sexo: String,
+    val telefono: String,
+    val ciudad: String,
+    val ocupacion: String,
+    val recomendadoPor: String,
+    val fechaRegistro: String,
+)
+
